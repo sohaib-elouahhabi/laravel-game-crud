ec6fbf760f9c802c636e224fe6114fb3372cb403
elouahhabi-sohaib <elouahhabisohaib@gmail.com>
2023-12-15 21:09:07 +0100

    first commit

---
+++ resources/js/main.js
@@ -0,0 +1,5 @@
+var headerCarousel = new bootstrap.Carousel(document.getElementById('headerCarousel'), {
+            interval: 2000, // Change image every 3 seconds
+            wrap: true,
+            keyboard: false
+        });
