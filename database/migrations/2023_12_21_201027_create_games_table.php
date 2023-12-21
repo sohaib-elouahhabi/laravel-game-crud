9d3bf5fb066ba0145d3ad157164230d54e6438ad
elouahhabi-sohaib <elouahhabisohaib@gmail.com>
2023-12-21 21:20:59 +0100

    add foriegn key to game table

---
+++ database/migrations/2023_12_21_201027_create_games_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->String('game_name');
+            $table->date('release_date');
+            $table->foreignId('catg_id')
+                ->constrained()
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+};
